4b2df89d1860ed55206aebe5542e902378d30df9
Tim Nguyen <tim.thien@yahoo.com>
2015-11-11 10:58:03 -0800

    instantiate new DonutShop location objects

--- donut.js
+++ donut.js
@@ -8,11 +8,20 @@ function DonutShop (location, minimum, maximum, average) {
   this.donutDemand = function() {
     var perHourDemand;
     var total = 0;
+    //loop through each hour from 7am - 6pm, 12 hours
     for(var i = 0; i < 12; i++) {
+      // donut demand per hour
       perHourDemand = (Math.floor(Math.random() * (this.maximum - this.minimum)) + this.minimum) * this.average;
-      total += perHourDemand;
+      total += perHourDemand; //hourly total
       console.log(perHourDemand);
     }
     console.log(total);
   };
 }
+
+//instantiate new DonutShop location objects
+var downtownDonutShop = new DonutShop("Downtown", 8, 43, 4.50);
+var capitolHillDonutShop = new DonutShop("Capitol Hill", 4, 37, 2.00);
+var southLakeUnionDonutShop = new DonutShop("South Lake Union", 9, 23, 6.33);
+var wedgewoodDonutShop = new DonutShop("Wedgewood", 2, 28, 1.25);
+var ballardDonutShop = new DonutShop("Ballard", 8, 58, 3.75);
