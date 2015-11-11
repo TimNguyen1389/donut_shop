9c07c82438512ed0306fc5d7dd17416561f5fc33
Tim Nguyen <tim.thien@yahoo.com>
2015-11-11 10:19:19 -0800

    add method to calculate hourly donut demand and total

--- donut.js
+++ donut.js
@@ -5,4 +5,14 @@ function DonutShop (location, minimum, maximum, average) {
   this.minimum = minimum;
   this.maximum = maximum;
   this.average = average;
+  this.donutDemand = function() {
+    var perHourDemand;
+    var total = 0;
+    for(var i = 0; i < 12; i++) {
+      perHourDemand = (Math.floor(Math.random() * (this.maximum - this.minimum)) + this.minimum) * this.average;
+      total += perHourDemand;
+      console.log(perHourDemand);
+    }
+    console.log(total);
+  };
 }
