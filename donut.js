ab59a0b3566afaf27d992f18315780ed7f219bd0
Tim Nguyen <tim.thien@yahoo.com>
2015-11-11 09:42:35 -0800

    add DonutShop constructor funtion

---
+++ donut.js
@@ -0,0 +1,8 @@
+// object constructor - location, minimum customer per hour,
+// maximum customers per hour, average donuts per customer.
+function DonutShop (location, minimum, maximum, average) {
+  this.location = location;
+  this.minimum = minimum;
+  this.maximum = maximum;
+  this.average = average;
+}
