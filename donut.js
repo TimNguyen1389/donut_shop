e7c14e8624251c5822480472607b30e3b740b239
Tim Nguyen <tim.thien@yahoo.com>
2015-11-11 12:08:37 -0800

    add HTML table and hours of operation row via JS

--- donut.js
+++ donut.js
@@ -25,3 +25,24 @@ var capitolHillDonutShop = new DonutShop("Capitol Hill", 4, 37, 2.00);
 var southLakeUnionDonutShop = new DonutShop("South Lake Union", 9, 23, 6.33);
 var wedgewoodDonutShop = new DonutShop("Wedgewood", 2, 28, 1.25);
 var ballardDonutShop = new DonutShop("Ballard", 8, 58, 3.75);
+
+// hours of operation in an array
+var hours = ["Location", "7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM"];
+
+// create table with hours of operation
+var donutTable = document.createElement('table');
+var row = document.createElement('tr');
+hours.forEach(function(hour) {
+  var td1 = document.createElement('td');
+  td1.textContent = hour;
+  row.appendChild(td1);
+  donutTable.appendChild(row);
+})
+document.body.appendChild(donutTable);
+
+
+
+
+
+
+
