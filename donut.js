7ee972d17afcc3b66ae02caff9ddf56d276e8cea
Tim Nguyen <tim.thien@yahoo.com>
2015-11-15 20:00:28 -0800

    using the same HTML exlements to update existing location

--- donut.js
+++ donut.js
@@ -33,6 +33,7 @@ function DonutShop (locale, minimum, maximum, average) {
 var hours = ["Location", "7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "Total"];
 
 // create table with location, hours of operation, and Total headings
+function makeTable(){
 var donutTable = document.createElement('table');
 var row = document.createElement('tr');
 hours.forEach(function(hour) {
@@ -43,23 +44,33 @@ hours.forEach(function(hour) {
 });
 // insert table to html body
 document.body.appendChild(donutTable);
+}
+makeTable();
 
 //instantiate new DonutShop location objects
-var downtownDonutShop = new DonutShop("Downtown", 8, 43, 4.50);
-var capitolHillDonutShop = new DonutShop("Capitol Hill", 4, 37, 2.00);
-var southLakeUnionDonutShop = new DonutShop("South Lake Union", 9, 23, 6.33);
-var wedgewoodDonutShop = new DonutShop("Wedgewood", 2, 28, 1.25);
-var ballardDonutShop = new DonutShop("Ballard", 8, 58, 3.75);
+var downtownDS = new DonutShop("Downtown", 8, 43, 4.50);
+var capitolHillDS = new DonutShop("Capitol Hill", 4, 37, 2.00);
+var southLakeUnionDS = new DonutShop("South Lake Union", 9, 23, 6.33);
+var wedgewoodDS = new DonutShop("Wedgewood", 2, 28, 1.25);
+var ballardDS = new DonutShop("Ballard", 8, 58, 3.75);
 
-// call object methods
-ballardDonutShop.donutDemand();
-wedgewoodDonutShop.donutDemand();
-southLakeUnionDonutShop.donutDemand();
-capitolHillDonutShop.donutDemand();
-downtownDonutShop.donutDemand();
+// array to hold DonutShop objects
+var donutShopArr = [];
+donutShopArr.push(ballardDS);
+donutShopArr.push(wedgewoodDS);
+donutShopArr.push(southLakeUnionDS);
+donutShopArr.push(capitolHillDS);
+donutShopArr.push(downtownDS);
 
+// to call DonutShop.donutDemand method for each object in array
+function renderDonutShopArr(){
+  for(var i = 0; i < donutShopArr.length; i++) {
+    donutShopArr[i].donutDemand();
+  }
+}
+renderDonutShopArr();
 
-// this function handles submission of new donut location
+// this function handles submission of new donut shop location form
 var handleLocationSubmit = function(event){
   event.preventDefault();
   var place = event.target.place.value;
@@ -67,19 +78,47 @@ var handleLocationSubmit = function(event){
   var maxCust = event.target.maxCust.value;
   var averagePur = event.target.averagePur.value;
 
+// to clear form after add/update location
+function clearForm(){
+  event.target.place.value = null;
+  event.target.minCust.value = null;
+  event.target.maxCust.value = null;
+  event.target.averagePur.value = null;
+}
+
   if(!place  || (!minCust || isNaN(minCust)) || (!maxCust || isNaN(maxCust)) || (!averagePur || isNaN(averagePur))) {
     alert('Fields cannot be empty and Minimum, Maximum, and Average fields must be a number!');
-  } else {
+  }
+  // match location name for updating
+  for(var i = 0; i < donutShopArr.length; i++){
+            if(place === donutShopArr[i].locale){
+              var match = 1;
+            }
+  }
+  // update location manimum, maximum and average if location matched
+  if (match === 1){
+    for(i = 0; i < donutShopArr.length; i++){
+      if(place === donutShopArr[i].locale){
+        donutShopArr[i].minimum = minCust;
+        donutShopArr[i].maximum = maxCust;
+        donutShopArr[i].average = averagePur;
+        document.getElementsByTagName('table')[0].remove(); //remove table
+        makeTable(); //create table and table headers
+        renderDonutShopArr(); //render rows and data
+        clearForm(); //clear form
+      }
+    }
+  }
+  else {
+    // add new location
     var newLocation = new DonutShop(place, minCust, maxCust, averagePur);
-    newLocation.donutDemand();
-
-    event.target.place.value = null;
-    event.target.minCust.value = null;
-    event.target.maxCust.value = null;
-    event.target.averagePur.value = null;
+    newLocation.donutDemand(); //render new location row and data
+    donutShopArr.push(newLocation); //push new location object into array
+    clearForm(); //clear form
   }
 };
 
+// Form and button event listener
 var locationForm = document.getElementById('location-form');
 locationForm.addEventListener('submit', handleLocationSubmit);
 
